ad45e5f49f9993e6d2944bd021ca4d11d82ce0b2
mdylka1 <mdylka1@ford.com>
2022-05-21 17:18:48 +0100

    Adding project files

---
+++ Inshorts/InshortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InshortsAPIApp.swift
+//  InshortsAPI
+//
+//  Created by Michal Dylka on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct InshortsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeListView()
+        }
+    }
+}
---
+++ Inshorts/Views/HomeListView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  InshortsAPI
+//
+//  Created by Michal Dylka on 20/05/2022.
+//
+
+import SwiftUI
+
+struct HomeListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
